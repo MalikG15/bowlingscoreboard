b003193991531083441f787068863c5a6b2ace31
Malik Graham <malikgraham15@yahoo.com>
2016-11-29 17:15:40 -0500

    Separated tenth row implementation from other rows

--- scoreboard-calculator.js
+++ scoreboard-calculator.js
@@ -1,75 +1,62 @@
 $(document).ready(function() {
 
 	var scoretable = $('#scoreboard');
-	var scores = new Array(8, 1, 10, 5, 5, 8, 0, 10, 10, 9, 1, 8, 1, 9, 1, 10, 7, 2);
+	var scores = new Array(8, 1, 10, 5, 5, 8, 0, 10, 10, 9, 1, 8, 1, 9, 1, 9, 1, 2);
 
 	var frame = 1;
 	var score = 0;
 	var rowTenLoc = 1;
 	for (var x = 0; x < scores.length;) {
-		//scoretable.append("<tr><td>" + frame + "<td>");
 		if (scores[x] === 10) {
-			if (frame < 10) {
-				score += calculateNextTwoThrows(x) + 10;
-				scoretable.append("<tr><td>" + frame + "</td><td>X</td><td></td><td></td><td>" + score + "</td></tr>");
-			}
-			else {
-				$('#' + rowTenLoc++).text("X");
-				score += 10;
-				$("#4").text(score);
-			}
+			score += calculateNextTwoThrows(x) + 10;
+			scoretable.append("<tr><td>" + frame + "</td><td>X</td><td></td><td></td><td>" + score + "</td></tr>");
 			x++;
 		}
 		else if (x + 1 < scores.length) {
 			if ((scores[x] + scores[x + 1] === 10)) {
-				//alert(scores[x + 1]);
-				if (frame < 10) {
-					score += calculateNextThrow(x + 2) + 10;
-					scoretable.append("<tr><td>" + frame + "</td><td>" + scores[x] + "</td><td>/</td><td></td><td>" + score + "</td></tr>");	
-				}
-				else {
-					$('#' + rowTenLoc++).text(scores[x]);
-					$('#' + rowTenLoc++).text("/");
-					score += 10;
-					$("#4").text(score);
-				}
+				score += calculateNextThrow(x + 2) + 10;
+				scoretable.append("<tr><td>" + frame + "</td><td>" + scores[x] + "</td><td>/</td><td></td><td>" + score + "</td></tr>");	
 			}
 			else {
-				//alert(scores[x + 1]);
-				if (frame < 10) {
-					score += scores[x] + scores[x + 1];
-					scoretable.append("<tr><td>" + frame + "</td><td>" + scores[x] + "</td><td>" + scores[x + 1] + "</td><td></td><td>" + score + "</td></tr>");
-				}
-				else {
-					$('#' + rowTenLoc++).text(scores[x]);
-					$('#' + rowTenLoc++).text(scores[x + 1]);
-					score += scores[x] + scores[x + 1];
-					$("#4").text(score);
-				}
+				score += scores[x] + scores[x + 1];
+				scoretable.append("<tr><td>" + frame + "</td><td>" + scores[x] + "</td><td>" + scores[x + 1] + "</td><td></td><td>" + score + "</td></tr>");
 			}
 			x += 2;
 		}
 		else {
 			score += scores[x];
-			if (frame < 10) {
-				scoretable.append("<tr><td>" + frame + "</td><td>" + scores[x] + "</td><td></td><td></td><td>" + score + "</td></tr>");
-			}
-			else {
-				$('#' + rowTenLoc++).text(scores[x]);
-				$("#4").text(score);
-			}
+			scoretable.append("<tr><td>" + frame + "</td><td>" + scores[x] + "</td><td></td><td></td><td>" + score + "</td></tr>");
 			x++;
 		}
-		//else break;
-
 
 		if (frame < 10) frame++;
-		if (frame == 10 && x < scores.length && rowTenLoc === 1) { 
+		if (frame == 10 && x < scores.length) { 
 			scoretable.append("<tr><td>10</td><td id='1'></td><td id='2'></td><td id='3'></td><td id='4'></td></tr>"); 
+			completeTenthRow(x);
+			break;
 		}
+	}
 
-
-
+	function completeTenthRow(index) {
+		for (var x = index; x < scores.length;) {
+			if (scores[x] === 10) {
+				score += 10;
+				$('#' + rowTenLoc++).text("X");
+				x++;
+			}
+			else if (x + 1 < scores.length && (scores[x] + scores[x + 1] === 10)) {
+					score += 10;
+					$('#' + rowTenLoc++).text(scores[x]);
+					$('#' + rowTenLoc++).text("/");
+					x += 2;
+			}
+			else {
+				score += scores[x];
+				$('#' + rowTenLoc++).text(scores[x]);
+				x++;
+			}
+		}
+		$("#4").text(score);
 	}
 	
 	function calculateNextTwoThrows(index) {
